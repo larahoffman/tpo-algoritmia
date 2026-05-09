46b935acfa7da41d9a8d6bd65f969f9d3c94d4c8
larahoffman <larahoff02@gmail.com>
2026-05-09 12:56:04 -0300

    agregado funcion login

--- login.py
+++ login.py
@@ -1,5 +1,14 @@
-usuario = "admin"
-contrasenia = "Admin2026!"
+def login(usuario, contrasenia):
+    acceso = False
+    while usuario != usuario_admin or contrasenia != contrasenia_admin:
+        print("Error! Usuario o contraseña incorrectos. Intente nuevamente.")
+        usuario = input("Ingrese su usuario: ")
+        contrasenia = input("Ingrese su contraseña: ")
+    acceso = True
+    return acceso
+
+usuario_admin = "admin"
+contrasenia_admin = "Admin2026!"
 
 print("--------------- Sistema de Venta de Pasajes ---------------")
 print("--------------------- Inicio de sesión --------------------")
@@ -7,21 +16,17 @@ print("--------------------- Inicio de sesión --------------------")
 user = input("Ingrese su usuario: ")
 password = input("Ingrese su contraseña: ")
 
-while user != usuario or password != contrasenia:
-    print("Error! Usuario o contraseña incorrectos. Intente nuevamente.")
-    user = input("Ingrese su usuario: ")
-    password = input("Ingrese su contraseña: ")
-
-print("--------------- Sistema de Venta de Pasajes ---------------")
-print("Bienvenido,", user)
-print('''
-----------------------
-|    Menú principal  |
-|    1. Clientes     |
-|    2. Destinos     |
-|    3. Pasajes      |
-----------------------
-    '''
-)
+if login(user, password):
+    print("--------------- Sistema de Venta de Pasajes ---------------")
+    print("Bienvenido,", usuario_admin)
+    print('''
+    ----------------------
+    |    Menú principal  |
+    |    1. Clientes     |
+    |    2. Destinos     |
+    |    3. Pasajes      |
+    ----------------------
+        '''
+    )
 
 
